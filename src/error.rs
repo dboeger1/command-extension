4cba95347659c0e8f91d82b34fb31863afc70c97
David Michael Boeger <dmboeger@gmail.com>
2024-01-25 23:27:06 +0000

    Clean up error.

--- src/error.rs
+++ src/error.rs
@@ -13,11 +13,4 @@ impl Display for Error {
     }
 }
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self
-            .source
-            .as_ref()
-            .map(|error| error.as_ref())
-    }
-}
+impl std::error::Error for Error {}
