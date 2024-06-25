8197a89cde9395d542a4c5a60175cc084935a50e
David Michael Boeger <dmboeger@gmail.com>
2024-06-25 18:24:58 +0000

    Changed from prun() to a trait.

--- src/lib.rs
+++ src/lib.rs
@@ -9,84 +9,98 @@ use std::{
     str::from_utf8,
 };
 
-pub use crate::error::Error;
+pub use error::Error;
 
 
-pub fn prun(
-    command: &mut Command,
-    print_on_success: bool,
-) -> Result<Output, Error> {
-    // Assemble command string.
-    let mut command_string = command
-        .get_program()
-        .to_os_string();
-    command
-        .get_args()
-        .for_each(|arg| command_string.push(format!(
-            " {}",
-            arg.to_string_lossy(),
-        )));
+pub struct ExecuteParameters {
+    pub print_on_success: bool,
+}
+pub struct ExecuteResult {
+    pub output: Output,
+}
 
-    // Run command.
-    let command_output = command
-        .output()
-        .map_err(|error| Error {
-            message: format!(
-                "Failed to execute command: {}",
+pub trait CommandExtension {
+    fn execute(&mut self, parameters: &ExecuteParameters)
+        -> Result<ExecuteResult, Error>;
+}
+
+impl CommandExtension for Command {
+    fn execute(&mut self, parameters: &ExecuteParameters)
+        -> Result<ExecuteResult, Error> {
+        // Assemble command string.
+        let mut command_string = self
+            .get_program()
+            .to_os_string();
+        self
+            .get_args()
+            .for_each(|arg| command_string.push(format!(
+                " {}",
+                arg.to_string_lossy(),
+            )));
+
+        // Run command.
+        let command_output = self
+            .output()
+            .map_err(|error| Error {
+                message: format!(
+                    "Failed to execute command: {}",
+                    command_string.to_string_lossy(),
+                ),
+                source: Some(Box::new(error)),
+            })?;
+        let command_success = command_output.status.success();
+
+        // Print command output.
+        if !command_success || parameters.print_on_success {
+            // Select between standard and error output.
+            let print = |message: &str| match command_success {
+                true => println!("{message}"),
+                false => eprintln!("{message}"),
+            };
+
+            // Print command string.
+            print("==== Command ====");
+            print(format!(
+                "{}",
                 command_string.to_string_lossy(),
-            ),
-            source: Some(Box::new(error)),
-        })?;
-    let command_success = command_output.status.success();
+            ).as_str());
 
-    // Print command output.
-    if !command_success || print_on_success {
-        // Select between standard and error output.
-        let print = |message: &str| match command_success {
-            true => println!("{message}"),
-            false => eprintln!("{message}"),
-        };
-
-        // Print command string.
-        print("==== Command ====");
-        print(format!(
-            "{}",
-            command_string.to_string_lossy(),
-        ).as_str());
-
-        // Print command exit code.
-        let command_exit_code = command_output
-            .status
-            .code();
-        print("==== Exit Code ====");
-        print(format!(
-            "{}",
-            command_exit_code.map_or_else(
-                || "<failed to retrieve>".to_string(),
-                |status| status.to_string(),
-            )
-        ).as_str());
-
-        // Print command stdout.
-        let command_stdout = from_utf8(&command_output.stdout);
-        print("==== stdout ====");
-        if command_stdout.is_ok() {
+            // Print command exit code.
+            let command_exit_code = command_output
+                .status
+                .code();
+            print("==== Exit Code ====");
             print(format!(
                 "{}",
-                command_stdout.unwrap_or("<failed to retrieve>"),
+                command_exit_code.map_or_else(
+                    || "<failed to retrieve>".to_string(),
+                    |status| status.to_string(),
+                )
             ).as_str());
+
+            // Print command stdout.
+            let command_stdout = from_utf8(&command_output.stdout);
+            print("==== stdout ====");
+            if command_stdout.is_ok() {
+                print(format!(
+                    "{}",
+                    command_stdout.unwrap_or("<failed to retrieve>"),
+                ).as_str());
+            }
+
+            // Print command stderr.
+            let command_stderr = from_utf8(&command_output.stderr);
+            print("==== stderr ====");
+            if command_stderr.is_ok() {
+                print(format!(
+                    "{}",
+                    command_stderr.unwrap_or("<failed to retrieve>"),
+                ).as_str());
+            }
         }
 
-        // Print command stderr.
-        let command_stderr = from_utf8(&command_output.stderr);
-        print("==== stderr ====");
-        if command_stderr.is_ok() {
-            print(format!(
-                "{}",
-                command_stderr.unwrap_or("<failed to retrieve>"),
-            ).as_str());
-        }
+        Ok(ExecuteResult {
+            output: command_output,
+        })
     }
-
-    Ok(command_output)
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,38 +6,54 @@ use std::process::Command;
 fn test_success_print_on_success_false() {
     // Should not print.
     let mut john_cena = Command::new("echo");
-    john_cena.arg("You can't see me!");
-    let result = prun(&mut john_cena, false);
+    john_cena.arg("John Cena: You can't see me!");
+
+    println!();
+    let result = john_cena.execute(&ExecuteParameters {
+        print_on_success: false,
+    });
     assert!(result.is_ok());
-    assert!(result.unwrap().status.success());
+    assert!(result.unwrap().output.status.success());
 }
 
 #[test]
 fn test_success_print_on_success_true() {
     // Should print.
     let mut dutch = Command::new("echo");
-    dutch.arg("I'm here! Come on! Do it!");
-    let result = prun(&mut dutch, true);
+    dutch.arg("Dutch: I'm here! Come on! Do it!");
+
+    println!();
+    let result = dutch.execute(&ExecuteParameters {
+        print_on_success: true,
+    });
     assert!(result.is_ok());
-    assert!(result.unwrap().status.success());
+    assert!(result.unwrap().output.status.success());
 }
 
 #[test]
 fn test_failure_print_on_success_false() {
     // Should print.
     let mut pinocchio = Command::new("cat");
-    pinocchio.arg("I'm a real boy!");
-    let result = prun(&mut pinocchio, false);
+    pinocchio.arg("Pinnochio: I'm a real boy!");
+
+    println!();
+    let result = pinocchio.execute(&ExecuteParameters {
+        print_on_success: false,
+    });
     assert!(result.is_ok());
-    assert!(!result.unwrap().status.success());
+    assert!(!result.unwrap().output.status.success());
 }
 
 #[test]
 fn test_failure_print_on_success_true() {
     // Should print.
     let mut pinocchio = Command::new("cat");
-    pinocchio.arg("I'm a real boy!");
-    let result = prun(&mut pinocchio, true);
+    pinocchio.arg("Pinnochio: I'm a real boy!");
+
+    println!();
+    let result = pinocchio.execute(&ExecuteParameters {
+        print_on_success: true,
+    });
     assert!(result.is_ok());
-    assert!(!result.unwrap().status.success());
+    assert!(!result.unwrap().output.status.success());
 }
